6e73501a8372573efa626efbdb4bbf779791bfb2
Loli Delgado <madodela@gmail.com>
2016-09-18 16:03:48 -0500

    Adding immediately-invoked function expressions

--- test/pig-latin-spec.js
+++ test/pig-latin-spec.js
@@ -1,21 +1,25 @@
-let expect = chai.expect
+(function () {
+  'use strict';
 
-describe('Pig Latin', function () {
+  let expect = chai.expect
 
-  it('Receive a string as input', function () {
-    let pigLatin = new PigLatin('hello')
-    expect(pigLatin.getPhrase()).to.be.a('string')
+  describe('Pig Latin', function () {
+
+    it('Receive a string as input', function () {
+      let pigLatin = new PigLatin('hello')
+      expect(pigLatin.getPhrase()).to.be.a('string')
+    })
+
+    it('The string can be a list of words separated by spaces', function () {
+      let pigLatin = new PigLatin('hello world')
+      expect(pigLatin.phraseHasSpaces()).to.not.be.false
+    })
+
+    it('The string can be reset without re-initializing', function () {
+      let pigLatin = new PigLatin('hello world')
+      expect(pigLatin.setPhrase).to.exist
+      pigLatin.setPhrase('good bye world')
+      expect(pigLatin.getPhrase()).to.equal('good bye world')
+    })
   })
-
-  it('The string can be a list of words separated by spaces', function () {
-    let pigLatin = new PigLatin('hello world')
-    expect(pigLatin.phraseHasSpaces()).to.not.be.false
-  })
-
-  it('The string can be reset without re-initializing', function () {
-    let pigLatin = new PigLatin('hello world')
-    expect(pigLatin.setPhrase).to.exist
-    pigLatin.setPhrase('good bye world')
-    expect(pigLatin.getPhrase()).to.equal('good bye world')
-  })
-})
+})()
