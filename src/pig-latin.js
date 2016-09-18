cc3a28e18666785fd1079b51740ac8b635fe75f0
Loli Delgado <madodela@gmail.com>
2016-09-18 15:22:53 -0500

    The string can have blank spaces

--- src/pig-latin.js
+++ src/pig-latin.js
@@ -3,4 +3,8 @@ function PigLatin (_phrase = "") {
   this.getPhrase = function () {
     return _phrase
   }
+
+  this.phraseHasSpaces = function () {
+    return /\s/g.test(_phrase)
+  }
 }
--- test/pig-latin-spec.js
+++ test/pig-latin-spec.js
@@ -2,8 +2,13 @@ let expect = chai.expect
 
 describe('Pig Latin', function () {
 
-  it('should receive a string as input', function () {
+  it('Receive a string as input', function () {
     let pigLatin = new PigLatin('hello')
-      expect(pigLatin.getPhrase()).to.be.a('string')
+    expect(pigLatin.getPhrase()).to.be.a('string')
+  })
+
+  it('The string can be a list of words separated by spaces', function () {
+    let pigLatin = new PigLatin('hello world')
+    expect(pigLatin.phraseHasSpaces()).to.not.be.false
   })
 })
