746ddbd13678c7637bafbfbac9f642c7e911ac98
madodela <madodela@gmail.com>
2016-09-19 11:55:46 -0500

    Moving translator functionality to piglatin class

--- src/pig-latin.js
+++ src/pig-latin.js
@@ -1,4 +1,7 @@
 function PigLatin (_phrase = "") {
+  
+  const vowels = /[aeiou]/i,
+      alphabet = /[b-z]/i
 
   this.getPhrase = function () {
     return _phrase
@@ -10,5 +13,84 @@ function PigLatin (_phrase = "") {
 
   this.hasSpacesOrHyphens = function () {
     return /\s|\-/g.test(_phrase)
+  }
+  
+  this.toPigLatin = function () {
+    let _englishPhrase = this.getPhrase()
+      if (!_englishPhrase) {
+        return null;
+      }
+
+      if (this.hasSpacesOrHyphens()) {
+        let words = _englishPhrase.match(/[^\s-]+-?/g),
+          pigLatinWords = []
+
+        words.forEach(word => {
+          let pigLatinWord = translate(word)
+          pigLatinWords.push(pigLatinWord)
+          if (!pigLatinWord.includes('-')) {
+              pigLatinWords.push(" ")
+          }
+        })
+
+        return pigLatinWords.join("").trim();
+      } else {
+        return translate(_englishPhrase);
+      }
+    }
+  
+  function translate (word) {
+    let firstLetter = word[0],
+        lastLetter = word[word.length -1],
+        isUpperCase = /[A-Z]/.test(firstLetter),
+        punctuation = /\W+/.test(word),
+        startSign = "",
+        endSign = ""
+
+    if (punctuation) {
+      startSign = word.split(/\w/).shift()
+      endSign = word.split(/\w/).pop()
+      word = word.split("").slice(startSign.length, word.length - endSign.length)
+      word = word.join("")
+      lastLetter = word[word.length - endSign.length]
+      firstLetter = word[0]
+    }
+
+    if (isVowel(firstLetter)) {
+      if (isConsonant(lastLetter) && lastLetter !== 'y') {
+        word += 'ay'
+      }
+      if (isVowel(lastLetter)) {
+        word += 'yay'
+      }
+      if (lastLetter === 'y') {
+        word += 'nay'
+      }
+    }
+    if (isConsonant(firstLetter)) {
+      let wordArray = word.split(""),
+        constantsTotal = word.indexOf(vowels.exec(word)),
+        constants = constantsTotal >= 0 ? wordArray.slice(0, constantsTotal) : []
+      wordArray.push(constants.join(""))
+      wordArray.push('ay')
+      wordArray.splice(0, constantsTotal)
+      word = wordArray.join("");
+    }
+    if (isUpperCase) {
+      word = word.toLowerCase()
+      word = word[0].toUpperCase() + word.split("").splice(1, word.length)
+      word = word.split(",").join("")
+    }
+    if (punctuation) {
+      word = startSign + word + endSign
+    }
+      return word
+  }
+
+  function isVowel (letter) {
+    return vowels.test(letter)
+  }
+  function isConsonant (letter) {
+    return alphabet.test(letter) && !vowels.test(letter)
   }
 }
--- test/pig-latin-spec.js
+++ test/pig-latin-spec.js
@@ -1,27 +1,90 @@
 (function () {
-  'use strict';
+  "use strict";
 
   let expect = chai.expect
 
-  describe('Pig Latin', function () {
+  describe("Pig Latin", function () {
 
-    it('Receive a string as input', function () {
-      let pigLatin = new PigLatin('hello')
-      expect(pigLatin.getPhrase()).to.be.a('string')
+    it("Receive a string as input", function () {
+      let pigLatin = new PigLatin("hello")
+      expect(pigLatin.getPhrase()).to.be.a("string")
     })
 
-    it('The string can be a list of words separated by spaces or hyphens', function () {
-      let pigLatin = new PigLatin('hello world')
+    it("The string can be a list of words separated by spaces or hyphens", function () {
+      let pigLatin = new PigLatin("hello world")
       expect(pigLatin.hasSpacesOrHyphens()).to.not.be.false
-      pigLatin.setPhrase('hello-world')
+      pigLatin.setPhrase("hello-world")
       expect(pigLatin.hasSpacesOrHyphens()).to.not.be.false
     })
 
-    it('The string can be reset without re-initializing', function () {
-      let pigLatin = new PigLatin('hello world')
+    it("The string can be reset without re-initializing", function () {
+      let pigLatin = new PigLatin("hello world")
       expect(pigLatin.setPhrase).to.exist
-      pigLatin.setPhrase('good bye world')
-      expect(pigLatin.getPhrase()).to.equal('good bye world')
+      pigLatin.setPhrase("good bye world")
+      expect(pigLatin.getPhrase()).to.equal("good bye world")
     })
+    
+    describe("Translator", function () {
+      let englishToPig
+      
+      beforeEach(function () {
+        englishToPig = new PigLatin()
+      })
+       
+      it("Translates empty string to null", function () {
+        expect(englishToPig.toPigLatin()).to.be.null
+      })
+
+      it("Supports any number of words in the phrase", function () {
+        englishToPig.setPhrase("hello-world")
+        expect(englishToPig.toPigLatin()).to.equal("ellohay-orldway")
+      })
+
+      it("Supports capital letters", function () {
+        englishToPig.setPhrase("Andrew")
+        expect(englishToPig.toPigLatin()).to.equal("Andreway")
+      })
+
+      it("Retain punctuation", function () {
+        englishToPig.setPhrase("Oh my god!")
+        expect(englishToPig.toPigLatin()).to.equal("Ohay myay odgay!")
+      })
+    
+      it("Retain spaces and hyphen in their place", function () {
+          englishToPig.setPhrase("More than one-third of registered voters oppose the measure.")
+          expect(englishToPig.toPigLatin()).to.equal("Oremay anthay oneyay-irdthay ofay egisteredray otersvay opposeyay ethay easuremay.")
+      })
+
+      describe("Translate words that start with vowels", function () {
+        it("Appending 'ay' to the word if it ends in a consonant", function () {
+          englishToPig.setPhrase("ask")
+          expect(englishToPig.toPigLatin()).to.equal("askay")
+        })
+        it("Appending 'yay' to the word if it ends with vowel", function () {
+          englishToPig.setPhrase("apple")
+          expect(englishToPig.toPigLatin()).to.equal("appleyay")
+        })
+        it("Appending 'nay' to the word if it ends with 'y'", function () {
+          englishToPig.setPhrase("any")
+          expect(englishToPig.toPigLatin()).to.equal("anynay")
+        })
+      })
+
+      describe("Translate words that start with a single consonant", function() {
+        it("Removing the starting consonant, adding it at the end and appeding 'ay' to the end",
+          function () {
+            englishToPig.setPhrase("hello")
+            expect(englishToPig.toPigLatin()).to.equal("ellohay")
+          })
+      })
+
+      describe("Translate words that start with multiple consonants", function () {
+        it("Removing all consonants at the front of the word, adding them  and 'ay' at the end",
+          function () {
+            englishToPig.setPhrase("known"),
+            expect(englishToPig.toPigLatin()).to.equal("ownknay")
+          })
+      })
+    })  
   })
 })()
